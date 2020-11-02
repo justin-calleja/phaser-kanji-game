92427932be3131294ec1e564db2f71321c895444
Justin Calleja <calleja.justin@gmail.com>
2020-11-02 10:11:07 +0100

    save

---
+++ libs/webstore/src/lib/webstore.spec.ts
@@ -0,0 +1,7 @@
+import { webstore } from './webstore';
+
+describe('webstore', () => {
+  it('should work', () => {
+    expect(webstore()).toEqual('webstore');
+  });
+});
---
+++ libs/webstore/src/lib/webstore.ts
@@ -0,0 +1,3 @@
+export function webstore(): string {
+  return 'webstore';
+}
